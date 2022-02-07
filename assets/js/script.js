9a714f55de6a5efbc12fc0aee754882aee75078b
Neo <fullstackneo@gmail.com>
2022-02-07 13:58:31 -0700

    handler function adn create function in form section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,35 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
   event.preventDefault();
-
-  var taskNameInput = document.querySelector("input[name='task-name']");
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
+  // create li item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = taskNameInput.value;
 
   // create div
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
   // li后面加div
   listItemEl.appendChild(taskInfoEl);
-
   // ul后面加li
   tasksToDoEl.appendChild(listItemEl);
 };
 
-// buttonEl.addEventListener("click", createTaskHandler);
-formEl.addEventListener("submit", createTaskHandler);
+// buttonEl.addEventListener("click", taskFormHandler);
+formEl.addEventListener("submit", taskFormHandler);
