c26532cd96404716c2222102d5b3dfbfba200a81
Neo <fullstackneo@gmail.com>
2022-02-07 13:27:53 -0700

    form area update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
-// var buttonEl = document.querySelector("#save-task");
-
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
-  // console.log(event);
   event.preventDefault();
 
   var taskNameInput = document.querySelector("input[name='task-name']");
-  console.log(taskNameInput);
-  console.dir(taskNameInput);
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
   var listItemEl = document.createElement("li");
-  listItemEl.textContent = document.querySelector("input[name='task-name']").value;
   listItemEl.className = "task-item";
+  listItemEl.textContent = taskNameInput.value;
+
+  // create div
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  // li后面加div
+  listItemEl.appendChild(taskInfoEl);
+
+  // ul后面加li
   tasksToDoEl.appendChild(listItemEl);
 };
 
